956791bed00012296cd2fd2ed1d126ef6fa12896
Brayan Salas Gonzalez <brayan@mscode.cl>
2021-04-10 18:58:40 -0400

    adaptacion

--- model/model_usuario.php
+++ model/model_usuario.php
@@ -5,12 +5,12 @@ class UsuarioModel
 {
 
     private $rut;
-    private $dv;
     private $nombre;
     private $apellido;
-    private $direccion;
-    private $password;
+    private $correo;
     private $estado;
+    private $tipo;
+    private $password;
 
     private $create;
     private $update;
@@ -41,8 +41,8 @@ class UsuarioModel
     public function lista(){
         try {
 
-            $sql = "SELECT rut_usuario, dv_usuario, nombres_usuario, apellidoPaterno_usuario, apellidoMaterno_usuario, password_usuario, estado_usuario
-            FROM tb_usuarios
+            $sql = "SELECT rut_usuario,nombre_usuario,apellido_usuario,correo_usuario,estado_usuario,tipo_usuario,password_usuario
+            FROM tb_usuario
             {$this->get('condicion')}";
             $datos = $this->con->consultaRetorno($sql);
             return $datos;
@@ -64,16 +64,16 @@ class UsuarioModel
     }
 
     public function add(){
-        $sql = "INSERT INTO tb_usuarios
-                (rut_usuario, dv_usuario, nombres_usuario, apellidoPaterno_usuario, apellidoMaterno_usuario, estado_usuario, password_usuario)
+        $sql = "INSERT INTO tb_usuario
+                (rut_usuario, nombre_usuario, apellido_usuario, correo_usuario, estado_usuario, tipo_usuario, password_usuario)
                 VALUES
                 (
                 '{$this->get('rut')}',
-                '{$this->get('dv')}',
                 '{$this->get('nombre')}',
-                '{$this->get('apellidop')}',
-                '{$this->get('apellidom')}',
+                '{$this->get('apellido')}',
+                '{$this->get('correo')}',
                 '{$this->get('estado')}',
+                '{$this->get('tipo')}',
                 '{$this->get('password')}'
                 )";
         $datos = $this->con->consultaRetorno($sql);
@@ -87,14 +87,14 @@ class UsuarioModel
     }
 
     public function edit(){
-        $sql = "UPDATE tb_usuarios
+        $sql = "UPDATE tb_usuario
                 SET
-                rut_usuario = '{$this->get('rut')}', 
-                dv_usuario = '{$this->get('dv')}',
-                nombres_usuario = '{$this->get('nombre')}',
-                apellidoPaterno_usuario = '{$this->get('apellidop')}',
-                apellidoMaterno_usuario = '{$this->get('apellidom')}',
-                password_usuario = '{$this->get('password')}'
+                rut_usuario = '{$this->get('rut')}',
+                nombre_usuario = '{$this->get('nombre')}',
+                apellido_usuario = '{$this->get('apellido')}',
+                correo_usuario = '{$this->get('correo')}',
+                tipo_usuario = '{$this->get('tipo')}',
+                password = '{$this->get('password')}'
                 WHERE
                 rut_usuario = '{$this->get('idusu')}'";
         $datos = $this->con->consultaRetorno($sql);
@@ -102,14 +102,14 @@ class UsuarioModel
     }
 
     public function estado(){
-        $sql = "UPDATE tb_usuarios
+        $sql = "UPDATE tb_usuario
                 SET estado_usuario = '{$this->get('estado')}'
                 WHERE rut_usuario = '{$this->get('id')}'";
         $this->con->consultaSimple($sql);
     }
 
     public function cambiaPw(){
-        $sql = "UPDATE tb_usuarios
+        $sql = "UPDATE tb_usuario
                 SET password_usuario = '{$this->get('pass')}'
                 WHERE rut_usuario = '{$this->get('rut_usuario')}'";
         $this->con->consultaSimple($sql);
