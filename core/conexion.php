5f3876ff446c2533cc10d68066c7bd575b8fd857
Brayan Salas Gonzalez <brayan@mscode.cl>
2021-04-16 00:08:37 -0400

    Update conexion.php

--- core/conexion.php
+++ core/conexion.php
@@ -5,7 +5,7 @@ class Conexion
         "host" => "localhost",
         "user" => "root",
         "pass" => "",
-        "db"   => "controlventas",
+        "db"   => "bd_controlventas",
         //"host" => "localhost",
         //"user" => "root",
         //"pass" => "",
